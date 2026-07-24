2ee8fcfb08fd70793ef9e5faddcffddc14899c3a
Dhruv Srivastava <dhruvrmcf@gmail.com>
2026-07-24 12:13:47 +0530

    Initialised Tokenizer

---
+++ gpt2/gpt2/tokenizer.hpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#pragma once
+
+class tokenizer {
+private:
+  std::unordered_map<std::string, int> token_to_int;
+  std::unordered_map<int, std::string> int_to_token;
+
+public:
+  int get_id(std::string word) {
+    auto it = token_to_int.find(word);
+    if (it == token_to_int.end()) {
+      throw std::runtime_error("Word Not Found");
+    }
+    return it->second;
+  }
+};
